6bb95f7a8ffcf001bf2d39c5d5a55e5a4f41c633
Luke Horwell <luke.horwell@digitalme.co.uk>
2018-03-08 14:35:03 +0000

    Add patch to suggest domain for usernames.

--- run-patches.sh
+++ run-patches.sh
@@ -12,6 +12,7 @@ function doPatch() {
 
 doPatch 0001-Prevent-activation-emails.patch /var/www/discourse/app/mailers/user_notifications.rb
 doPatch 0002-Force-set-email-from-line.patch /var/www/discourse/app/mailers/user_notifications.rb
+doPatch 0003-Suggest-domain-for-usernames.patch /var/www/discourse/spec/components/user_name_suggester_spec.rb
 
 echo "-------------------------------"
 echo "End of Patches"
