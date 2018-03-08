6023adb82c784b7c2e91d819c7a1bf876bbcfa23
Luke Horwell <luke.horwell@digitalme.co.uk>
2018-03-08 14:36:52 +0000

    Add patch to suggest longer usernames.

--- run-patches.sh
+++ run-patches.sh
@@ -13,8 +13,8 @@ function doPatch() {
 doPatch 0001-Prevent-activation-emails.patch /var/www/discourse/app/mailers/user_notifications.rb
 doPatch 0002-Force-set-email-from-line.patch /var/www/discourse/app/mailers/user_notifications.rb
 doPatch 0003-Suggest-domain-for-usernames.patch /var/www/discourse/spec/components/user_name_suggester_spec.rb
+doPatch 0004-Increase-username-suggestions.patch /var/www/discourse/spec/components/user_name_suggester_spec.rb
 
 echo "-------------------------------"
 echo "End of Patches"
 echo "-------------------------------"
-
