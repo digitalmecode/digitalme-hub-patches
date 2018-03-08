861cafcc802c08c77bda4ec82c4bfc722beeabbe
Luke Horwell <luke.horwell@digitalme.co.uk>
2018-03-08 14:20:24 +0000

    Add patch to force "from" in user e-mail notifications.

--- run-patches.sh
+++ run-patches.sh
@@ -11,6 +11,7 @@ function doPatch() {
 }
 
 doPatch 0001-Prevent-activation-emails.patch /var/www/discourse/app/mailers/user_notifications.rb
+doPatch 0002-Force-set-email-from-line.patch /var/www/discourse/app/mailers/user_notifications.rb
 
 echo "-------------------------------"
 echo "End of Patches"
