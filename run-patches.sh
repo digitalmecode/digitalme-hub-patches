bbfe186989eaa52781a4840b76b58e25727129d9
Luke Horwell <luke.horwell@digitalme.co.uk>
2018-03-15 10:01:31 +0000

    Add patch to ignore e-mail vertification during login.

--- run-patches.sh
+++ run-patches.sh
@@ -16,7 +16,9 @@ doPatch 0001-Prevent-activation-emails.patch /var/www/discourse/app/mailers/user
 doPatch 0002-Force-set-email-from-line.patch /var/www/discourse/app/mailers/user_notifications.rb
 doPatch 0003-Suggest-domain-for-usernames.patch /var/www/discourse/spec/components/user_name_suggester_spec.rb
 doPatch 0004-Increase-username-suggestions.patch /var/www/discourse/spec/components/user_name_suggester_spec.rb
+doPatch 0005-Ignore-email-activation-check.patch /var/www/discourse/app/controllers/session_controller.rb
 
 echo "-------------------------------"
 echo "End of Patches"
 echo "-------------------------------"
+
